fb77dd84a7db1456131648f76ec54004827333a7
MrRobot2211 <felipebihaiek@gmail.com>
2021-06-18 03:34:36 -0300

    added minimal test for proper cupy installation

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,3 +4,19 @@
 def test_import():
     import qutip_cupy
     assert qutip_cupy.__version__
+
+
+def test_cupy_import():
+    import cupy as cp
+    pauli_x = cp.array([[0., 1.],
+                        [1., 0.]], dtype=cp.complex64)
+
+    pauli_y = cp.array([[0., -1.j],
+                        [1.j, 0]], dtype=cp.complex64)
+
+    commutator_x_z = pauli_x @ pauli_y - (pauli_y@pauli_x)
+
+    _2j_pauli_z = cp.array([[2.j, 0.j],
+                           [0.j, -2.j]], dtype=cp.complex64)
+
+    assert (commutator_x_z == _2j_pauli_z).all()
