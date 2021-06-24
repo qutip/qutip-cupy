8d74a1af2a82500d62c868d3ec5ceed5075f1058
MrRobot2211 <felipebihaiek@gmail.com>
2021-06-24 15:20:14 -0300

    tests: changed name and refactor mark as fixture

--- tests/test_base_dense.py
+++ tests/test_base_dense.py
@@ -2,13 +2,12 @@ from qutip_cupy import CuPyDense
 import numpy as np
 import pytest
 
+@pytest.fixture(scope="function", params=((1, 2), (5, 10), (7, 3), (2, 5)))
+def shape(request):
+    return request.param
 
-def test_import():
-    import qutip_cupy
-    assert qutip_cupy.__version__
 
 
-@pytest.mark.parametrize("shape", ((1, 2), (5, 10), (7, 3), (2, 5)))
 def test_conversion_cycle(shape):
 
     from qutip.core import data
@@ -16,18 +15,18 @@ def test_conversion_cycle(shape):
     qutip_dense = data.Dense(np.random.uniform(size=shape))
 
     tr1 = data.to(CuPyDense, qutip_dense)
-    tr2 =  data.to(data.Dense, tr1)
+    tr2 = data.to(data.Dense, tr1)
 
     assert (qutip_dense.to_array() == tr2.to_array()).all()
 
-@pytest.mark.parametrize("shape", ((1, 2), (5, 10), (7, 3), (2, 5)))
+
 def test_shape(shape):
 
-     cupy_dense = CuPyDense(np.random.uniform(size=shape))
+    cupy_dense = CuPyDense(np.random.uniform(size=shape))
 
-     assert (cupy_dense.shape == shape)
+    assert (cupy_dense.shape == shape)
 
-@pytest.mark.parametrize("shape", ((1, 2), (5, 10), (7, 3), (2, 5)))
+
 def test_adjoint(shape):
 
     from qutip.core import data
@@ -41,8 +40,8 @@ def test_adjoint(shape):
 
 
 
-@pytest.mark.parametrize(["matrix",'trace'], [pytest.param([[0,1],[1,0]], 0),
-                                              pytest.param([[2.j,1],[1,1]], 1+2.j)])
+@pytest.mark.parametrize(["matrix", "trace"], [pytest.param([[0, 1],[1, 0]], 0),
+                                              pytest.param([[2.j, 1],[1, 1]], 1+2.j)])
 def test_trace(matrix, trace):
 
     cupy_array = CuPyDense(matrix)
