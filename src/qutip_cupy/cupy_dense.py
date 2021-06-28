1265ccbe2af29c4ccb85a2cfe9f922e0b6e33982
MrRobot2211 <felipebihaiek@gmail.com>
2021-06-28 15:03:03 -0300

    clean-up

--- src/qutip_cupy/cupy_dense.py
+++ src/qutip_cupy/cupy_dense.py
@@ -94,9 +94,9 @@ class CuPyDense(data.Data):
     def __itruediv__(self, other):
         if not isinstance(other, numbers.Number):
             return NotImplemented
-        cdef int size = self.shape[0]*self.shape[1]
-        cdef double complex mul = 1 / complex(other)
-        blas.zscal(&size, &mul, self.data, &_ONE)
+        # cdef int size = self.shape[0]*self.shape[1]
+        # cdef double complex mul = 1 / complex(other)
+        # blas.zscal(&size, &mul, self.data, &_ONE)
         return self
 
     def __neg__(self):
@@ -119,22 +119,18 @@ def empty(rows, cols, fortran):
     Return a new Dense type of the given shape, with the data allocated but
     uninitialised.
     """
-    # cdef Dense out = Dense.__new__(Dense)
-    # out.shape = (rows, cols)
-    # out.data = <double complex *> PyDataMem_NEW(rows * cols * sizeof(double complex))
-    # out._deallocate = True
-    # out.fortran = fortran
-    out = CuPyDense.__new__()
-    out.shape = (rows, cols)
+    out = CuPyDense.__new__(CuPyDense)
+    super(CuPyDense, out).__init__(data.shape)
     order = 'F' if fortran else 'C'
     out._cp = cp.empty(shape=(rows, cols), dtype=cp.complex128, order=order)
 
     return CuPyDense(out)
 
 
-def empty_like(other):
+def empty_like(other, fortran):
     out = CuPyDense.__new__()
     out.shape = other.shape
+    order = 'F' if fortran else 'C'
     out._cp = cp.empty_like(other, dtype=cp.complex128, order=order)
 
     return CuPyDense(out)
@@ -142,14 +138,8 @@ def empty_like(other):
 
 def zeros(rows, cols, fortran):
     """Return the zero matrix with the given shape."""
-    # cdef Dense out = Dense.__new__(Dense)
-    # out.shape = (rows, cols)
-    # out.data =\
-    #     <double complex *> PyDataMem_NEW_ZEROED(rows * cols, sizeof(double complex))
-    # out.fortran = fortran
-    # out._deallocate = True
-    out = CuPyDense.__new__()
-    out.shape = (rows, cols)
+    out = CuPyDense.__new__(CuPyDense)
+    super(CuPyDense, out).__init__(data.shape)
     order = 'F' if fortran else 'C'
     out._cp = cp.zeros(shape=(rows, cols), dtype=cp.complex128, order=order)
     return out
@@ -161,8 +151,8 @@ def identity(dimension, scale=1, fortran=True):
     the diagonal.  By default this will be the identity matrix, but if `scale`
     is passed, then the result will be `scale` times the identity.
     """
-    out = CuPyDense.__new__()
-    out.shape = (dimension, dimension)
+    out = CuPyDense.__new__(CuPyDense)
+    super(CuPyDense, out).__init__(data.shape)
     order = 'F' if fortran else 'C'
     if scale != 1:
         out._cp = cp.eye(dimension, dtype=cp.complex128,order=orde)
@@ -172,32 +162,10 @@ def identity(dimension, scale=1, fortran=True):
     return out
 
 
-def Dense from_csr(CSR matrix, bint fortran=False):
-    cdef Dense out = Dense.__new__(Dense)
-    out.shape = matrix.shape
-    out.data = (
-        <double complex *>
-        PyDataMem_NEW_ZEROED(out.shape[0]*out.shape[1], sizeof(double complex))
-    )
-    out.fortran = fortran
-    out._deallocate = True
-    cdef size_t row, ptr_in, ptr_out, row_stride, col_stride
-    row_stride = 1 if fortran else out.shape[1]
-    col_stride = out.shape[0] if fortran else 1
-    ptr_out = 0
-    for row in range(out.shape[0]):
-        for ptr_in in range(matrix.row_index[row], matrix.row_index[row + 1]):
-            out.data[ptr_out + matrix.col_index[ptr_in]*col_stride] = matrix.data[ptr_in]
-        ptr_out += row_stride
-    return out
+def Dense_from_csr(CSR_matrix, fortran=False):
+    pass
 
 
-# def inline base.idxint _diagonal_length(
-#     base.idxint offset, base.idxint n_rows, base.idxint n_cols,
-# ) nogil:
-#     if offset > 0:
-#         return n_rows if offset <= n_cols - n_rows else n_cols - offset
-#     return n_cols if offset > n_cols - n_rows else n_rows + offset
 
 def diags(diagonals, offsets=None, shape=None):
     """
@@ -225,75 +193,76 @@ def diags(diagonals, offsets=None, shape=None):
         not need to be square, but the diagonals must be of the correct length
         to fit in exactly.
     """
-    cdef base.idxint n_rows, n_cols, offset
-    try:
-        diagonals = list(diagonals)
-        if diagonals and np.isscalar(diagonals[0]):
-            # Catch the case where we're being called as (for example)
-            #   diags([1, 2, 3], 0)
-            # with a single diagonal and offset.
-            diagonals = [diagonals]
-    except TypeError:
-        raise TypeError("diagonals must be a list of arrays of complex") from None
-    if offsets is None:
-        if len(diagonals) == 0:
-            offsets = []
-        elif len(diagonals) == 1:
-            offsets = [0]
-        else:
-            raise TypeError("offsets must be supplied if passing more than one diagonal")
-    offsets = np.atleast_1d(offsets)
-    if offsets.ndim > 1:
-        raise ValueError("offsets must be a 1D array of integers")
-    if len(diagonals) != len(offsets):
-        raise ValueError("number of diagonals does not match number of offsets")
-    if len(diagonals) == 0:
-        if shape is None:
-            raise ValueError("cannot construct matrix with no diagonals without a shape")
-        else:
-            n_rows, n_cols = shape
-        return zeros(n_rows, n_cols)
-    order = np.argsort(offsets)
-    diagonals_ = []
-    offsets_ = []
-    prev, cur = None, None
-    for i in order:
-        cur = offsets[i]
-        if cur == prev:
-            diagonals_[-1] += np.asarray(diagonals[i], dtype=np.complex128)
-        else:
-            offsets_.append(cur)
-            diagonals_.append(np.asarray(diagonals[i], dtype=np.complex128))
-        prev = cur
-    if shape is None:
-        n_rows = n_cols = abs(offsets_[0]) + len(diagonals_[0])
-    else:
-        try:
-            n_rows, n_cols = shape
-        except (TypeError, ValueError):
-            raise TypeError("shape must be a 2-tuple of positive integers")
-        if n_rows < 0 or n_cols < 0:
-            raise ValueError("shape must be a 2-tuple of positive integers")
-    for i in range(len(diagonals_)):
-        offset = offsets_[i]
-        if len(diagonals_[i]) != _diagonal_length(offset, n_rows, n_cols):
-            raise ValueError("given diagonals do not have the correct lengths")
-    if n_rows == 0 and n_cols == 0:
-        raise ValueError("can't produce a 0x0 matrix")
+    # cdef base.idxint n_rows, n_cols, offset
+    # try:
+    #     diagonals = list(diagonals)
+    #     if diagonals and np.isscalar(diagonals[0]):
+    #         # Catch the case where we're being called as (for example)
+    #         #   diags([1, 2, 3], 0)
+    #         # with a single diagonal and offset.
+    #         diagonals = [diagonals]
+    # except TypeError:
+    #     raise TypeError("diagonals must be a list of arrays of complex") from None
+    # if offsets is None:
+    #     if len(diagonals) == 0:
+    #         offsets = []
+    #     elif len(diagonals) == 1:
+    #         offsets = [0]
+    #     else:
+    #         raise TypeError("offsets must be supplied if passing more than one diagonal")
+    # offsets = np.atleast_1d(offsets)
+    # if offsets.ndim > 1:
+    #     raise ValueError("offsets must be a 1D array of integers")
+    # if len(diagonals) != len(offsets):
+    #     raise ValueError("number of diagonals does not match number of offsets")
+    # if len(diagonals) == 0:
+    #     if shape is None:
+    #         raise ValueError("cannot construct matrix with no diagonals without a shape")
+    #     else:
+    #         n_rows, n_cols = shape
+    #     return zeros(n_rows, n_cols)
+    # order = np.argsort(offsets)
+    # diagonals_ = []
+    # offsets_ = []
+    # prev, cur = None, None
+    # for i in order:
+    #     cur = offsets[i]
+    #     if cur == prev:
+    #         diagonals_[-1] += np.asarray(diagonals[i], dtype=np.complex128)
+    #     else:
+    #         offsets_.append(cur)
+    #         diagonals_.append(np.asarray(diagonals[i], dtype=np.complex128))
+    #     prev = cur
+    # if shape is None:
+    #     n_rows = n_cols = abs(offsets_[0]) + len(diagonals_[0])
+    # else:
+    #     try:
+    #         n_rows, n_cols = shape
+    #     except (TypeError, ValueError):
+    #         raise TypeError("shape must be a 2-tuple of positive integers")
+    #     if n_rows < 0 or n_cols < 0:
+    #         raise ValueError("shape must be a 2-tuple of positive integers")
+    # for i in range(len(diagonals_)):
+    #     offset = offsets_[i]
+    #     if len(diagonals_[i]) != _diagonal_length(offset, n_rows, n_cols):
+    #         raise ValueError("given diagonals do not have the correct lengths")
+    # if n_rows == 0 and n_cols == 0:
+    #     raise ValueError("can't produce a 0x0 matrix")
 
-    out = zeros(n_rows, n_cols, fortran=True)
+    # out = zeros(n_rows, n_cols, fortran=True)
 
-    cdef size_t diag_idx, idx, n_diagonals = len(diagonals_)
+    # cdef size_t diag_idx, idx, n_diagonals = len(diagonals_)
 
-    for diag_idx in range(n_diagonals):
-        offset = offsets_[diag_idx]
-        if offset <= 0:
-            for idx in range(_diagonal_length(offset, n_rows, n_cols)):
-                out.data[idx*(n_rows+1) - offset] = diagonals_[diag_idx][idx]
-        else:
-            for idx in range(_diagonal_length(offset, n_rows, n_cols)):
-                out.data[idx*(n_rows+1) + offset*n_rows] = diagonals_[diag_idx][idx]
-    return out
+    # for diag_idx in range(n_diagonals):
+    #     offset = offsets_[diag_idx]
+    #     if offset <= 0:
+    #         for idx in range(_diagonal_length(offset, n_rows, n_cols)):
+    #             out.data[idx*(n_rows+1) - offset] = diagonals_[diag_idx][idx]
+    #     else:
+    #         for idx in range(_diagonal_length(offset, n_rows, n_cols)):
+    #             out.data[idx*(n_rows+1) + offset*n_rows] = diagonals_[diag_idx][idx]
+    # return out
+    pass
 
 
 
