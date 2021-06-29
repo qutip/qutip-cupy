5cc66899362c03c36801d4e884f47f73a36d2fed
MrRobot2211 <felipebihaiek@gmail.com>
2021-06-29 20:20:21 -0300

    Refactors __init__ adds docstring, dense deletion after use and sorts imports properly

--- src/qutip_cupy/__init__.py
+++ src/qutip_cupy/__init__.py
@@ -1,25 +1,30 @@
-from .version import version as __version__
-from .cupy_dense import *
-
-
+""" The qutip-cupy package provides a CuPy-based data layer for QuTiP. """
 from qutip.core import data
+
+from .version import version as __version__
+from . import dense as cd
+
+CuPyDense = cd.CuPyDense
+
 data.to.add_conversions([
-     (CuPyDense, data.Dense, cupydense_from_dense),
-     (data.Dense, CuPyDense, dense_from_cupydense),
+     (CuPyDense, data.Dense, cd.cupydense_from_dense),
+     (data.Dense, CuPyDense, cd.dense_from_cupydense),
  ])
 data.to.register_aliases(['cupyd'], CuPyDense)
 
 data.adjoint.add_specialisations([
-      (CuPyDense, CuPyDense, cpd_adjoint),
+      (CuPyDense, CuPyDense, cd.cpd_adjoint),
      ])
 data.transpose.add_specialisations([
-      (CuPyDense, CuPyDense, cpd_transpose),
+      (CuPyDense, CuPyDense, cd.cpd_transpose),
      ])
 data.conj.add_specialisations([
-      (CuPyDense, CuPyDense, cpd_conj),
+      (CuPyDense, CuPyDense, cd.cpd_conj),
      ])
 
 
-# We must register the functions to the data layer but do not want 
+# We must register the functions to the data layer but do not want
 # the data layer to be callable from qutip_cupy
 del data
+
+del cd
