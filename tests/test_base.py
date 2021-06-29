6aa3e7a9bf5fcfd79864d9a89bb0695158aa10a1
Simon Cross <hodgestar@gmail.com>
2021-06-29 17:11:12 +0200

    Add a test that uses cupy and the GPU.

--- tests/test_base.py
+++ tests/test_base.py
@@ -4,3 +4,17 @@
 def test_import():
     import qutip_cupy
     assert qutip_cupy.__version__
+
+
+def test_import_cupy():
+    import cupy
+    assert cupy.__version__
+
+
+def test_norm_cupy():
+    import cupy as cp
+    import numpy as np
+    x = cp.array([1, 2, 3])
+    y = cp.array([2, 3, 4])
+    z = x + y
+    assert z == np.array([3, 5, 7])
