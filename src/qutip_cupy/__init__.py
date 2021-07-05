72f52c7dfd8be00d92346b24bf5e66975ff83384
Felipe Bivort Haiek <felipebihaiek@gmail.com>
2021-07-05 16:24:09 -0300

    fixes extra space in init
    Co-authored-by: Simon Cross <hodgestar+github@gmail.com>

--- src/qutip_cupy/__init__.py
+++ src/qutip_cupy/__init__.py
@@ -21,20 +21,20 @@ from . import dense as cd
 CuPyDense = cd.CuPyDense
 
 data.to.add_conversions([
-     (CuPyDense, data.Dense, cd.cupydense_from_dense),
-     (data.Dense, CuPyDense, cd.dense_from_cupydense),
- ])
+    (CuPyDense, data.Dense, cd.cupydense_from_dense),
+    (data.Dense, CuPyDense, cd.dense_from_cupydense),
+])
 data.to.register_aliases(['cupyd'], CuPyDense)
 
 data.adjoint.add_specialisations([
-      (CuPyDense, CuPyDense, cd.cpd_adjoint),
-     ])
+    (CuPyDense, CuPyDense, cd.cpd_adjoint),
+])
 data.transpose.add_specialisations([
-      (CuPyDense, CuPyDense, cd.cpd_transpose),
-     ])
+    (CuPyDense, CuPyDense, cd.cpd_transpose),
+])
 data.conj.add_specialisations([
-      (CuPyDense, CuPyDense, cd.cpd_conj),
-     ])
+    (CuPyDense, CuPyDense, cd.cpd_conj),
+])
 data.trace.add_specialisations([
       (CuPyDense, cd.cpd_trace),
      ])
