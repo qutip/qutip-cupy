1c951e06f7f8c68f23e61a977e299a70c9b4c19e
MrRobot2211 <felipebihaiek@gmail.com>
2021-07-08 19:19:50 -0300

    adds div, mul, matmul and itruediv basic tests

--- tests/test_dense.py
+++ tests/test_dense.py
@@ -43,3 +43,58 @@ class TestCuPyDense:
     def test_trace(self, matrix, trace):
         cupy_array = CuPyDense(matrix)
         assert cupy_array.trace() == trace
+
+import time
+@pytest.mark.benchmark(
+    min_rounds=5,
+    timer=time.time,
+)
+def test_true_div(shape, benchmark):
+
+    from qutip.core import data
+
+    array = np.random.uniform(size=shape) + 1.j*np.random.uniform(size=shape)
+    
+    def divide_by_2(cp_arr):
+        return cp_arr /2.
+    cup_arr = CuPyDense(array)
+    cpdense_tr = benchmark(divide_by_2, cup_arr)
+    qtpdense_tr = data.Dense(array) /2.
+
+    assert (cpdense_tr.to_array() == qtpdense_tr.to_array()).all()
+
+
+def test_itrue_div(shape):
+
+    from qutip.core import data
+
+    array = np.random.uniform(size=shape) + 1.j*np.random.uniform(size=shape)
+
+    cpdense_tr = CuPyDense(array).__itruediv__(2.)
+    qtpdense_tr = data.Dense(array).__itruediv__(2.)
+
+    assert (cpdense_tr.to_array() == qtpdense_tr.to_array()).all()
+
+
+def test_mul(shape):
+
+    from qutip.core import data
+
+    array = np.random.uniform(size=shape) + 1.j*np.random.uniform(size=shape)
+
+    cpdense_tr = CuPyDense(array).__mul__(2.+1.j)
+    qtpdense_tr = data.Dense(array).__mul__(2.+1.j)
+
+    assert (cpdense_tr.to_array() == qtpdense_tr.to_array()).all()
+
+
+def test_matmul(shape):
+
+    from qutip.core import data
+
+    array = np.random.uniform(size=shape) + 1.j*np.random.uniform(size=shape)
+
+    cpdense_tr = CuPyDense(array).__mul__(2.+1.j)
+    qtpdense_tr = data.Dense(array).__mul__(2.+1.j)
+
+    assert (cpdense_tr.to_array() == qtpdense_tr.to_array()).all()
