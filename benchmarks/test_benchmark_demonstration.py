a6217ca3d6e066f8e0ebeb78beb916c7735e916a
MrRobot2211 <felipebihaiek@gmail.com>
2021-07-27 08:37:00 -0300

    add groups dynamically in benchmark tests

--- benchmarks/test_benchmark_demonstration.py
+++ benchmarks/test_benchmark_demonstration.py
@@ -3,10 +3,21 @@
 import pytest
 import numpy as np
 import cupy as cp
+import qutip as qt
+from qutip_cupy import CuPyDense
 
 # from qutip_cupy import CuPyDense
 
 from .cpu_gpu_times_wrapper import GpuWrapper
+
+# Supported dtypes
+dtype_list = [np, CuPyDense, qt.data.Dense, qt.data.CSR]
+dtype_ids = ["numpy", "CuPy", "qutip(Dense)", "qutip(CSR)"]
+
+
+@pytest.fixture(params=dtype_list, ids=dtype_ids)
+def dtype(request):
+    return request.param
 
 
 @pytest.fixture(scope="function", params=((1000, 1000)))  # ,(2000, 2000)))
@@ -15,7 +26,12 @@ def shape(request):
 
 
 @pytest.mark.benchmark()
-def test_matmul(shape, benchmark):
+def test_matmul(shape, benchmark, request):
+    # Group benchmark by operation, density and size.
+    group = request.node.callspec.id  # noqa:F821
+    group = group.split("-")
+    benchmark.group = "-".join(group[1:])
+    benchmark.extra_info["dtype"] = "complex"  # group[0]
 
     array = np.random.uniform(size=shape) + 1.0j * np.random.uniform(size=shape)
 
