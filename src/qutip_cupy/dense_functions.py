f40bad59b89e62c4460615797c5bfd6e9f2f0d34
MrRobot2211 <felipebihaiek@gmail.com>
2021-07-28 20:11:23 -0300

    add inner_op dense function

--- src/qutip_cupy/dense_functions.py
+++ src/qutip_cupy/dense_functions.py
@@ -62,3 +62,47 @@ def inner_cupydense(left, right, scalar_is_ket=False):
     # possible as it forces CPU GPU  transfers.
 
     return cp.vdot(left._cp, right._cp).item()
+
+
+def _check_shape_inner_op(left, op, right):
+    left_shape = left.shape[0] == 1 or left.shape[1] == 1
+    left_op = (left.shape[0] == 1 and left.shape[1] == op.shape[0]) or (
+        left.shape[1] == 1 and left.shape[0] == op.shape[0]
+    )
+    op_right = op.shape[1] == right.shape[0]
+    right_shape = right.shape[1] == 1
+    if not (left_shape and left_op and op_right and right_shape):
+        raise ValueError(
+            "".join(
+                [
+                    "incompatible matrix shapes ",
+                    str(left.shape),
+                    ", ",
+                    str(op.shape),
+                    " and ",
+                    str(right.shape),
+                ]
+            )
+        )
+
+
+def inner_op_cupydense(left, op, right, scalar_is_ket=False):
+
+    _check_shape_inner_op(left, op, right)
+
+    if (
+        left.shape[0]
+        == left.shape[1]
+        == op.shape[0]
+        == op.shape[1]
+        == right.shape[1]
+        == 1
+    ):
+        if cp.all(left._cp == 0) or cp.all(right._cp == 0) or cp.all(op._cp == 0):
+            return 0
+        lstate = cp.conj(left._cp[0]) if scalar_is_ket else left._cp[0]
+        return (lstate * op._cp[0] * right._cp[0]).item()
+
+    if left.shape[0] == 1:
+        return cp.vdot(cp.conj(left._cp), op._cp @ right._cp).item()
+    return cp.vdot(left._cp, op._cp @ right._cp).item()
