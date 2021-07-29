173ae60624d05132a476ea236d410716844f2361
MrRobot2211 <felipebihaiek@gmail.com>
2021-07-29 01:26:00 -0300

    dispatch frobenius and l2 functions

--- src/qutip_cupy/__init__.py
+++ src/qutip_cupy/__init__.py
@@ -58,6 +58,10 @@ data.tidyup.add_specialisations([(CuPyDense, cdf.tidyup_dense)])
 data.trace.add_specialisations([(CuPyDense, cdf.trace_cupydense)])
 data.reshape.add_specialisations([(CuPyDense, CuPyDense, cdf.reshape_cupydense)])
 
+# the whole norm module is imported in QuTiP
+# so the dispatching is peculiar
+data.norm.l2.add_specialisations([(CuPyDense, cdf.l2_cupydense)])
+data.norm.frobenius.add_specialisations([(CuPyDense, cdf.frobenius_cupydense)])
 # We must register the functions to the data layer but do not want
 # the data layer or qutip_cupy.dense to be accessible from qutip_cupy
 del data
