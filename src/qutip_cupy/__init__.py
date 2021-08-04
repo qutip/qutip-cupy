f9734696d983433a49af12a04824608aed95fa7d
MrRobot2211 <felipebihaiek@gmail.com>
2021-08-04 14:55:49 -0300

    dispatch is_herm

--- src/qutip_cupy/__init__.py
+++ src/qutip_cupy/__init__.py
@@ -58,6 +58,7 @@ data.tidyup.add_specialisations([(CuPyDense, cdf.tidyup_dense)])
 data.trace.add_specialisations([(CuPyDense, cdf.trace_cupydense)])
 data.reshape.add_specialisations([(CuPyDense, CuPyDense, cdf.reshape_cupydense)])
 
+data.is_herm.add_specialisations([(CuPyDense, cdf.isherm_cupydense)])
 # We must register the functions to the data layer but do not want
 # the data layer or qutip_cupy.dense to be accessible from qutip_cupy
 del data
