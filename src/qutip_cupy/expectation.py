43bb220d74754f8d27fa0a06cbc653c29e831c0b
MrRobot2211 <felipebihaiek@gmail.com>
2021-08-11 01:56:05 -0300

    clean-up

--- src/qutip_cupy/expectation.py
+++ src/qutip_cupy/expectation.py
@@ -1,20 +1,4 @@
 import cupy as cp
-from cupy import cublas
-
-
-def expect_dense_naive(op, state):
-    """
-    Get the expectation value of the operator `op` over the state `state`.  The
-    state can be either a ket or a density matrix.
-
-    The expectation of a state is defined as the operation:
-        state.adjoint() @ op @ state
-    and of a density matrix:
-        tr(op @ state)
-    """
-    if state.shape[1] == 1:
-        return _expect_dense_ket_naive(op, state)
-    return _expect_dense_dense_dm(op, state)
 
 
 def expect_cupydense(op, state):
@@ -28,27 +12,13 @@ def expect_cupydense(op, state):
         tr(op @ state)
     """
     if state.shape[1] == 1:
-        return _expect_dense_ket_naive2(op, state)
+        return _expect_dense_ket(op, state)
     return _expect_dense_dense_dm(op, state)
 
 
-def _expect_dense_ket_naive(op, state):
-
-    return state.adjoint() @ (op @ state)
-
-
-def _expect_dense_ket_naive2(op, state):
+def _expect_dense_ket(op, state):
     _check_shape_ket(op, state)
     return cp.vdot(state._cp, op._cp @ state._cp).item()
-
-
-def _expect_dense_ket_cublas(op, state):
-    _check_shape_ket(op, state)
-    out = cp.zeros((1, 1), cp.complex128)
-
-    cublas.gemm("H", "N", state._cp, op._cp @ state._cp, out=out)
-
-    return out.item()
 
 
 def _check_shape_dm(op, state):
@@ -62,7 +32,7 @@ def _check_shape_dm(op, state):
         )
 
 
-_expect_dens_kernel = cp.RawKernel(
+_expect_dense_kernel = cp.RawKernel(
     r"""
     #include <cupy/complex.cuh>
     extern "C" __global__
@@ -79,26 +49,15 @@ _expect_dens_kernel = cp.RawKernel(
 )
 
 
-def expect_cupydense_dm(cp_arr, dens):
-    size = cp_arr.shape[0]
+def _expect_dense_dense_dm(op, state):
+    _check_shape_dm(op, state)
+    size = op.shape[0]
     out = cp.zeros((size,), dtype=cp.complex128)
     # TODO: check if there is a better way to set thread dim and block dim
     block_size = 64
     grid_size = (size + block_size - 1) // block_size
-    _expect_dens_kernel((grid_size,), (block_size,), (cp_arr, dens, size, out))
-    return out.sum()
-
-
-def _expect_dense_dense_dm(op, state):
-    _check_shape_dm(op, state)
-
-    return expect_cupydense_dm(op._cp, state._cp).item()
-
-
-def _expect_dense_dense_dm_cupy(op, state):
-    _check_shape_dm(op, state)
-
-    return cp.sum(op._cp * state._cp.transpose()).item()
+    _expect_dense_kernel((grid_size,), (block_size,), (op._cp, state._cp, size, out))
+    return out.sum().item()
 
 
 def _check_shape_ket(op, state):
